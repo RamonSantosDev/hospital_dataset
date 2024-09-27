4076718f08e1cc9cefda8f6e8241569bb647e139
Ramon Santos <117384857+RamonSantosDev@users.noreply.github.com>
2024-09-27 09:37:58 -0300

    Delete Perguntas_GestaoHospital directory

--- Perguntas_GestaoHospital/Question2/Question2.sqlite.sql
+++
@@ -1,9 +0,0 @@
--- TABLE
-CREATE TABLE 'hospital_dataset' ('Hospital_ID' INTEGER,'Hospital_Name' TEXT,'Procedure_ID' INTEGER,'Procedure_Name' TEXT,'Cost' REAL,'Satisfaction_Rating' INTEGER,'Procedure_Date' TEXT,'Performance_Score' INTEGER,'Number_of_Procedures' INTEGER,'City' TEXT,'State' TEXT);
- 
--- INDEX
- 
--- TRIGGER
- 
--- VIEW
- 
